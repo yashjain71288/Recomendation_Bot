b74ab9f9934cb407fb185993ded344e9f7c93813
yash7@Burster <yash71288@gmail.com>
2025-02-24 00:07:59 +0530

    Your commit message

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 import streamlit as st
+import pickle
+
+movies = pickle.load(open("movies_list.pkl", 'rb'))
 
 st.header("Recomendation System")
+st.selectbox("Select Movies from Dropdown", movies)
+
+if st.button("Show Recomended"):
+    pass
